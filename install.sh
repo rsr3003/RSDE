f4c8ed162fcee78abef670844c25843f3905c0ac
rohan <rsrohan99@gmail.com>
2020-10-24 18:56:57 +0600

    Fixed typo

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo -e "\n..................................Done...............................
 echo -e "Now downloading packages related to xfce4 panel..." &&
 sudo apt install xfce4-panel xfce4-whiskermenu-plugin xfce4-notifyd xfce4-power-manager-plugins xfce4-indicator-plugin -y &&
 echo -e "\n..................................Done.....................................\n\n" &&
-echo -e "\nNow setting up all the configuration files....." &&
+echo -e "\nNow setting up all the configuration files....." && mkdir ~/.config &&
 cp .Xresources-RSDE ~/ && cp compton.conf ~/.config/ && cp -r .rsde ~/ && cp -r RSDE ~/.config/ &&
 echo -e "\n..................................Done.....................................\n\n" &&
 echo -e "\nNow setting up RSDE-gnome-flashback....." &&
@@ -27,7 +27,7 @@ NOTIFICATIONS=$(gsettings get org.gnome.gnome-flashback notifications)
 STATUS_NOTIFIER=$(gsettings get org.gnome.gnome-flashback status-notifier-watcher)
 SWITCH_IN_SRC=$(gsettings get org.gnome.desktop.wm.keybindings switch-input-source)
 SWITCH_IN_SRC_BCD=$(gsettings get org.gnome.desktop.wm.keybindings switch-input-source-backward)
-echo "gsettings set org.gnome.gnome-flashback desktop ${DESKTOP} && gsettings set org.gnome.gnome-flashback root-background ${ROOT_BACKGROUND} && gsettings set org.gnome.gnome-flashback notifications ${NOTIFICATIONS} && gsettings set org.gnome.gnome-flashback status-notifier-watcher ${STATUS_NOTIFIER} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source ${SWITCH_IN_SRC} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ${SWITCH_IN_SRC_BCD}" >> ~/.rsde.backup &&
-gsettings set org.gnome.gnome-flashback desktop false && gsettings set org.gnome.gnome-flashback root-background true && gsettings set org.gnome.gnome-flashback notifications false && gsettings set org.gnome.gnome-flashback status-notifier-watcher false && gsettings set org.gnome.desktop.wm.keybindings switch-input-source <Alt><Super>space && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward <Shift><Alt><Super>space
+echo "gsettings set org.gnome.gnome-flashback desktop ${DESKTOP} && gsettings set org.gnome.gnome-flashback root-background ${ROOT_BACKGROUND} && gsettings set org.gnome.gnome-flashback notifications ${NOTIFICATIONS} && gsettings set org.gnome.gnome-flashback status-notifier-watcher ${STATUS_NOTIFIER} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source ${SWITCH_IN_SRC} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ${SWITCH_IN_SRC_BCD}" > ~/.rsde.backup &&
+gsettings set org.gnome.gnome-flashback desktop false && gsettings set org.gnome.gnome-flashback root-background true && gsettings set org.gnome.gnome-flashback notifications false && gsettings set org.gnome.gnome-flashback status-notifier-watcher false && gsettings set org.gnome.desktop.wm.keybindings switch-input-source '[<Alt><Super>space]' && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward '[<Shift><Alt><Super>space]' &&
 echo -e "\n...........................RSDE is ready to go.............................\n" &&
 echo -e "\n................Now Reboot and select RSDE as your session.................\n"
--- install_wsl.sh
+++ install_wsl.sh
@@ -14,7 +14,7 @@ echo -e "\n..................................Done...............................
 echo -e "Now downloading packages related to xfce4 panel..." &&
 sudo apt install xfce4-panel xfce4-whiskermenu-plugin xfce4-notifyd xfce4-power-manager-plugins xfce4-indicator-plugin -y &&
 echo -e "\n..................................Done.....................................\n\n" &&
-echo -e "\nNow setting up all the configuration files....." &&
+echo -e "\nNow setting up all the configuration files....." && mkdir ~/.config &&
 cp .Xresources-RSDE ~/ && cp compton.conf ~/.config/ && cp -r .rsde ~/ && cp -r RSDE ~/.config/ &&
 echo -e "\n..................................Done.....................................\n\n" &&
 echo -e "\nNow setting up RSDE-gnome-flashback....." &&
@@ -27,8 +27,8 @@ NOTIFICATIONS=$(gsettings get org.gnome.gnome-flashback notifications)
 STATUS_NOTIFIER=$(gsettings get org.gnome.gnome-flashback status-notifier-watcher)
 SWITCH_IN_SRC=$(gsettings get org.gnome.desktop.wm.keybindings switch-input-source)
 SWITCH_IN_SRC_BCD=$(gsettings get org.gnome.desktop.wm.keybindings switch-input-source-backward)
-echo "gsettings set org.gnome.gnome-flashback desktop ${DESKTOP} && gsettings set org.gnome.gnome-flashback root-background ${ROOT_BACKGROUND} && gsettings set org.gnome.gnome-flashback notifications ${NOTIFICATIONS} && gsettings set org.gnome.gnome-flashback status-notifier-watcher ${STATUS_NOTIFIER} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source ${SWITCH_IN_SRC} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ${SWITCH_IN_SRC_BCD}" >> ~/.rsde_backup &&
-gsettings set org.gnome.gnome-flashback desktop false && gsettings set org.gnome.gnome-flashback root-background true && gsettings set org.gnome.gnome-flashback notifications false && gsettings set org.gnome.gnome-flashback status-notifier-watcher false && gsettings set org.gnome.desktop.wm.keybindings switch-input-source <Alt><Super>space && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward <Shift><Alt><Super>space
+echo "gsettings set org.gnome.gnome-flashback desktop ${DESKTOP} && gsettings set org.gnome.gnome-flashback root-background ${ROOT_BACKGROUND} && gsettings set org.gnome.gnome-flashback notifications ${NOTIFICATIONS} && gsettings set org.gnome.gnome-flashback status-notifier-watcher ${STATUS_NOTIFIER} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source ${SWITCH_IN_SRC} && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ${SWITCH_IN_SRC_BCD}" > ~/.rsde_backup &&
+gsettings set org.gnome.gnome-flashback desktop false && gsettings set org.gnome.gnome-flashback root-background true && gsettings set org.gnome.gnome-flashback notifications false && gsettings set org.gnome.gnome-flashback status-notifier-watcher false && gsettings set org.gnome.desktop.wm.keybindings switch-input-source '[<Alt><Super>space]' && gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ['<Shift><Alt><Super>space'] &&
 echo -e "\nNow Installing & applying the the default gtk & icon theme you can apply them later"
 sudo apt install materia-gtk-theme papirus-icon-theme -y
 echo -e "\nSetting up XRDP"
